25f491dc153325233d2c97e60ff51f38bf519619
Gilles Meier <krtek4@gmail.com>
2012-04-15 16:47:54 +0200

    format the data based on labels and attributes of each fields and create link to other models

--- classes/viewmodel_crud.php
+++ classes/viewmodel_crud.php
@@ -20,7 +20,7 @@ class View_Crud extends ViewModel_Base {
 			if(! isset($this->{$var_name}))
 				$var_name = \Fuel\Core\Inflector::pluralize($var_name);
 			if(isset($this->{$var_name}))
-				$this->data = $this->{$var_name};
+				$this->set('data', $this->format_data($this->{$var_name}), false);
 		}
 
 		return parent::render();
@@ -33,4 +33,52 @@ class View_Crud extends ViewModel_Base {
 		$this->form = $model_name::fieldset('admin')->repopulate();
 		$this->{$var_name}->populate($this->form);
 	}
+
+	protected function format_data($data) {
+		if(is_array($data))
+			return $data;
+
+		$model_name = $this->model;
+		$ret = '<table class="model_listing">
+			<thead>
+				<tr><th>Nom</th><th>Valeur</th></tr>
+			</thead>
+			<tbody>';
+		foreach($data as $name => $value) {
+			$label = $model_name::_labels($name, 'admin') ?: $name;
+			$attributes = $model_name::_attributes($name, 'default') ?: array('type' => 'text');
+
+			switch($attributes['type']) {
+				case 'date': $value = date('Y.m.d H:m', strtotime($value));
+					break;
+				case 'checkbox': $value = $value == 0 ? 'No' : 'Yes';
+					break;
+				case 'select':
+				case 'radio':
+					if(isset($attributes['options']) && isset($attributes['options'][$value]))
+						$value = $attributes['options'][$value];
+					break;
+			}
+
+			if(substr($name, -3) == '_id') {
+				$name = substr($name, 0, -3);
+				if(isset($attributes['callback'])) {
+					$model = $attributes['callback'][0];
+					$name = strtolower(str_replace('Model_', '', \Fuel\Core\Inflector::denamespace($model)));
+				} else
+					$model = 'Model_'.ucfirst($name);
+
+				$title = $model::find_by_pk($value);
+				if($title)
+					$title = $title->select_name();
+				else
+					$title = $value;
+				$value = '<a href="'.$this->base_url.'/../'.\Fuel\Core\Inflector::pluralize($name).'/view/'.$value.'">'.$title.'</a>';
+			}
+
+			$ret .= '<tr><td>'.$label.' :</td><td>'.$value.'</td></tr>';
+		}
+		return $ret.'</tbody></table>';
+	}
+
 }
