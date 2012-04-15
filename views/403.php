0f66573f4ebaf046780586462d2aa241b046a93c
Gilles Meier <krtek4@gmail.com>
2012-04-15 17:20:26 +0200

    tuned the default 403 error page some more

--- views/403.php
+++ views/403.php
@@ -1,4 +1,11 @@
-<h1>Forbidden</h1>
-
-<p>You don't have permission to access <?= $_SERVER['REQUEST_URI'] ?> on this server.</p>
-<hr>
+<!doctype html>
+<html>
+	<head>
+		<title>403: Forbidden</title>
+	</head>
+	<body>
+		<h1>Forbidden</h1>
+		<p>You don't have permission to access <?= $_SERVER['REQUEST_URI'] ?> on this server.</p>
+		<hr>
+	</body>
+</html>
